4e786e8d1c5b9b9aaa1a10e5cba00dffcb8826dd
Astyr <astyr.github@gmail.com>
2021-01-27 12:03:39 +0100

    Initial commit

---
+++ RmvAudio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace RmvAudio
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0) return; 
+            var path = args[0];
+            if (!path.EndsWith(".mp4")) path = args[0];
+            var outputPath = path.Replace(".mp4", "_muted.mp4");
+            Process.Start(new ProcessStartInfo("ffmpeg.exe", $"-i \"{path}\" -vcodec copy -an {outputPath}"));
+        }
+    }
+}
