d0715b22e3f3103f5c6dcc48e9d58e90520f11ec
Astyr <astyr.github@gmail.com>
2021-01-27 12:08:54 +0100

    Changed the output path renaming

--- RmvAudio/Program.cs
+++ RmvAudio/Program.cs
@@ -9,7 +9,7 @@ namespace RmvAudio
             if (args.Length == 0) return; 
             var path = args[0];
             if (!path.EndsWith(".mp4")) path = args[0];
-            var outputPath = path.Replace(".mp4", "_muted.mp4");
+            var outputPath = "Muted" + path;
             Process.Start(new ProcessStartInfo("ffmpeg.exe", $"-i \"{path}\" -vcodec copy -an {outputPath}"));
         }
     }
